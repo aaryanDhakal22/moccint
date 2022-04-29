f0639c2bb5afeb1be1b48d59a815af29f2873dea
aaryan.dhakal22 <aaryan.dhakal22@gmail.com>
2022-04-29 12:59:45 +0545

    single process added instead of bulk

--- main.py
+++ main.py
@@ -10,43 +10,36 @@ mic = sr.Microphone()
 
 nlp = spacy.load("en_core_web_lg")
 
-audio_answers = []
-audio_to_voice_answers = []
-for index,question in study.items():
+for index, records in study.items():
 
-    Qs = question[0]
-    As = question[1]
+    question = records[0]
+    answer = records[1]
 
     engine = pyttsx3.init()
-    engine.say(Qs)
+    engine.say(question)
     engine.runAndWait()
-
+    engine.stop()
+    
     print("Listening now")
 
-    with mic as source :
+    with mic as source:
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
 
-    audio_answers.append([Qs,As,audio])
-    print("\nDone listening")
-    if index == "03":
-        break
+    print("Done listening\n")
 
-for records in audio_answers:
-    audio_traslated = r.recognize_google(records[2])
-    audio_to_voice_answers.append([records[0],records[1],audio_traslated])
-    real_answers = nlp(records[1])
-    user_answers = nlp(audio_traslated)
+    audio_traslated = r.recognize_google(audio)
+    print("Done Translating")
+    nlp_answers = nlp(answer)
+    nlp_user_answers = nlp(audio_traslated)
 
-    similarity = real_answers.similarity(user_answers)
+    similarity = nlp_answers.similarity(nlp_user_answers)
 
-    if float(similarity) > 0.9 :
-        print("%sPASSED%s"%(fg(2),attr(0)),similarity)
+    if float(similarity) > 0.9:
+        print("%sPASSED%s" % (fg(2), attr(1)), similarity)
     else:
-        print("%sFAILED%s"%(fg(1),attr(0)),similarity)
+        print("%sFAILED%s" % (fg(1), attr(0)), similarity)
 
-
-for item in audio_to_voice_answers:
-    print("Q : "+item[0]+"\n")
-    print("A : "+item[1]+"\n")
-    print("Your : "+item[2]+"\n")
+    print("Q : " + question + "\n")
+    print("A : " + answer + "\n")
+    print("Your : " + audio_traslated + "\n")
